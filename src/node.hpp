0692344884b16592f47d6ca6887b5df4f6f8ca6c
mkornaukhov <mkornaukhov03@yandex.ru>
2021-10-17 13:07:16 +0300

    Refactoring

--- src/node.hpp
+++ src/node.hpp
@@ -5,7 +5,9 @@
 */
 #pragma once
 
-// #include <llvm/IR/Value.h>
+#include "decl.hpp"
+
+#include <llvm/IR/Value.h>
 
 #include <vector>
 #include <string>
@@ -18,6 +20,23 @@
 
 namespace AST
 {
+
+    namespace details
+    {
+        std::unordered_map<std::string, Identifier *> &Variables();
+
+        DataType GetType(int val);
+
+        DataType GetType(bool val);
+
+        DataType GetType(std::string val);
+
+        bool HasType(const Expression &e, DataType type);
+
+        bool SameType(const Expression &lhs, const Expression &rhs);
+        std::string ShowType(DataType type);
+    }
+
     // None must be unreachable
     enum class DataType
     {
@@ -50,60 +69,46 @@ namespace AST
         Or
     };
 
-    struct CodeGenContext;
-    struct Node;
-    struct Expression;
-    struct Statement;
-    struct Identifier;
-
-    using StatementList = std::vector<Statement *>;
-
     static StatementList CurCodeBlock;
-    // TODO make .hpp file with ^^^^^^^^^^
-
-    std::string ShowType(DataType type);
 
     std::unordered_map<std::string, Identifier *> &Variables();
-
-    void PrintVarDict();
 
     struct Node
     {
         virtual ~Node();
-        // virtual llvm::Value *CodeGen(CodeGenContext &context)
-        // {
-        //     return nullptr;
-        // }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context)
+        {
+            return nullptr;
+        }
     };
 
     struct Expression : Node
     {
         DataType type;
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
-    bool HasType(const Expression &e, DataType type);
+    // bool HasType(const Expression &e, DataType type);
 
-    bool SameType(const Expression &lhs, const Expression &rhs);
+    // bool SameType(const Expression &lhs, const Expression &rhs);
 
     struct Statement : Node
     {
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
     struct CodeBlock : Node
     {
         StatementList statements;
         CodeBlock(StatementList statements_);
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
     // Kinds of expression
 
-
-    DataType GetType(int val);
-    DataType GetType(bool val);
-    DataType GetType(std::string val);
+    // DataType GetType(int val);
+    // DataType GetType(bool val);
+    // DataType GetType(std::string val);
 
     template <class LiteralType>
     struct Constant : Expression
@@ -111,19 +116,18 @@ namespace AST
         LiteralType val;
         Constant(LiteralType val_) : val(val_)
         {
-            type = GetType(val_);
+            type = details::GetType(val_);
             std::cerr << "Constructed Constant: " << val_ << std::endl;
         }
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
-
 
     // Gonna find value in context
     struct Identifier : public Expression
     {
         std::string name;
         Identifier(DataType type, std::string name_);
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
     struct UnaryOp : public Expression
@@ -131,7 +135,7 @@ namespace AST
         UnaryOpType op;
         Expression &expr;
         UnaryOp(UnaryOpType op_, Expression &expr_);
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
     struct BinaryOp : public Expression
@@ -140,14 +144,14 @@ namespace AST
         Expression &lhs;
         Expression &rhs;
         BinaryOp(Expression &lhs_, BinaryOpType op_, Expression &rhs_);
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
     // Kinds of statement
 
     struct Skip : Statement
     {
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
     struct VarDecl : Statement
@@ -155,7 +159,7 @@ namespace AST
         Identifier *ident;
         Expression &expr;
         VarDecl(Identifier *ident_, Expression &expr_);
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
     struct VarAssign : Statement
@@ -163,7 +167,7 @@ namespace AST
         Identifier *ident;
         Expression &expr;
         VarAssign(std::string &ident_name, Expression &expr_);
-        // virtual llvm::Value *CodeGen(CodeGenContext &context) { return nullptr; }
+        virtual llvm::Value *CodeGen(codegen::CodeGenContext &context) { return nullptr; }
     };
 
     struct WhileLoop : Statement
